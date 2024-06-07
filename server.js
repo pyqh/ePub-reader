a9e301423594cacc1242a6fccd0edbce55f3d02b
pyqh <pyh8879@gmail.com>
2024-06-07 15:26:38 +0800

    add start.sh

--- server.js
+++ server.js
@@ -4,26 +4,24 @@ const path = require('path');
 
 const getContentType = (filePath) => {
   const extname = path.extname(filePath);
-  const mimeTypes = {
+  const contentType = {
     '.html': 'text/html',
     '.css': 'text/css',
     '.js': 'text/javascript',
     '.gif': 'image/gif',
     '.epub': 'application/epub+zip',
   };
-  return mimeTypes[extname] || 'text/plain';
+  return contentType[extname] || 'text/plain';
 };
 
 const server = http.createServer((req, res) => {
   const filePath = path.join(__dirname, req.url === '/' ? 'index.html' : decodeURI(req.url));
-
   fs.readFile(filePath, (err, data) => {
     if (err) {
       res.writeHead(404, { 'Content-Type': 'text/plain' });
-      res.end('Not Found');
+      res.end('404 (Not Found)');
       return;
     }
-
     res.writeHead(200, { 'Content-Type': getContentType(filePath) });
     res.end(data);
   });
